5ade3e9296272109e41ae94a43bf15d92fcdc79a
justinpolchinski <justinpolchinski@gmail.com>
2018-04-23 19:04:32 -0400

    Mangager js updated

--- bamazonManager.js
+++ bamazonManager.js
@@ -51,7 +51,7 @@ function inventory(){
                 break;
       
             case "Add New Product":
-              
+                addProduct();
                 break;
             case "Exit":
                 connection.end();
@@ -76,11 +76,11 @@ function lowInventory() {
     connection.query(query, function(err, res) {
         if (err) throw err;
             for (var i = 0; i < res.length; i++) {
-                if(res[i].stock_quantity<=15){
+                if(res[i].stock_quantity<=5){
                 console.log("\n===================\nProduct ID: " + res[i].item_id +"\nDepartment: " + res[i].department_name  + "\nProcuct Name: " + res[i].product_name + "\nPrice: " + res[i].price.toFixed(2) + "\nQuantity: " + res[i].stock_quantity + "\n====================");
                }
             }    
-            console.log("Press Arrow Key for Menu Options");
+            console.log("\nPress Arrow Key for Menu Options");
     });
     inventory();
 }
@@ -88,7 +88,7 @@ function addInventory() {
     inquirer.prompt({
         name: "name",
         type: "list",
-        message: "Which product should be restocked?",
+        message: "\nWhich product should be restocked?",
         choices: arrProduct
     }).then(function(answer){
           var query = "SELECT item_id, product_name, price, stock_quantity FROM products";
@@ -110,7 +110,7 @@ function updateQuantity(){
     inquirer.prompt({
         name: "quantity",
         type: "input",
-        message: "How many more items should be added to inventory?",
+        message: "\nHow many more items should be added to inventory?",
     }).then(function (answer1){
     orderQuantity = answer1.quantity;
     newStockQuantity = parseInt(inStock)+ parseInt(orderQuantity);
@@ -142,7 +142,52 @@ function productChoices(){
         for (var i = 0; i < res.length; i++) {
             arrProduct.unshift(res[i].product_name);
         }
-        //console.log(arrProduct);
         inventory();
         })
     }
+function addProduct(){
+    inquirer.prompt([{
+        name: "product_name",
+        type: "input",
+        message: "\nWhat is the name of new product?"
+    },
+    {
+        name: "department_name",
+        type: "input",
+        message: "\nWhich department will this be located?"
+    },
+    {
+        name: "price",
+        type: "input",
+        message: "\nWhat will be the sale price?",
+        validate: function(value) {
+            if(isNaN(value)===false){
+                return true;
+            }
+            return false;
+        }
+    },
+    {
+        name: "stock_quantity",
+        type: "input",
+        message: "\nHow many show be stocked?",
+        validate: function(value){
+            if(isNaN(value)===false){
+                return true;
+            }
+            return false;
+        }
+    }]).then(function (answer2){
+        var query = connection.query(
+            "INSERT INTO products SET ?",
+            {
+                product_name: answer2.product_name,
+                department_name: answer2.department_name,
+                price: answer2.price,
+                stock_quantity: answer2.stock_quantity
+            }
+        )
+        arrProduct.unshift(answer2.product_name);
+        inventory();
+    })
+}
